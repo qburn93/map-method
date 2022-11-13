d49221f42de7da3cf2d4e4a5af283efe39c39f9a
Bogdan <bogdan.gugu93@gmail.com>
2022-11-13 20:08:53 +0000

    Initial commit

--- map.js
+++ map.js
@@ -5,17 +5,29 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5 ];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+
+console.log(results);
 
 
 // Using map()
-
+const multByTwo = function (num) {
+  return num * 2;
+}
+const mapresults = nums.map(multByTwo);
+console.log(mapresults);
 
 // Simplified w/ map()
+const simplified = nums.map(function(num) { return num * 2 });
 
 
 // Simplfied w/ map() + arrow function
-
-
+const arrow = nums.map(num => num * 2);
+console.log(arrow);
 // With objects:
 const students = [
   {
@@ -37,3 +49,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithlds = students.map(student => [student.name + student.id]);
+console.log(studentsWithlds);
